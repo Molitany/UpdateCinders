621fc5039591d1c197ed474beb506d380cb8d260
Thor Beregaard <thor@beregaard.dk>
2021-03-29 14:26:11 +0200

    Cinders Auto Update Completed

--- UpdateCinders.py
+++ UpdateCinders.py
@@ -1,9 +1,15 @@
+from platform import version
+import sys
+import os
+import configparser
 import requests
+import zipfile
 import config
-from bs4 import BeautifulSoup
+from requests.api import request
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
+
 
 cinderVersion = requests.get('https://api.nexusmods.com/v1/games/darksouls3/mods/310.json', 
     headers = {
@@ -13,20 +19,25 @@ cinderVersion = requests.get('https://api.nexusmods.com/v1/games/darksouls3/mods
 )
 
 data = cinderVersion.json()
-file = open('version.txt', 'r')
+file = open('./version.txt', 'r')
 fileContents = file.read()
 
 if (data['version'] != fileContents):
+    with open('./version.txt', 'w') as file:
+        file.write(data['version'])
+        file.close()
     CinderFiles = requests.get('https://api.nexusmods.com/v1/games/darksouls3/mods/310/files.json?category=main', 
         headers = {
             'accept': 'application/json', 
             'apikey':'SkxEVGpsczRQMUxMckZZbVo3MzBKenN5d2J5SHhYMHpWSXVjWElMbjdQRT0tLXdsR3FqYXBNZWExUXBHTXNkM2hGV2c9PQ==--36a55a13f3a90230a584db021155dc8e23c7969d'
         }
     )
+
     data = CinderFiles.json()
     ids = []
     for p in data['files']:
-        ids.append(p['id'][0])
+        if ('Model' not in p['name']):
+            ids.append(p['id'][0])
     
     browser = webdriver.Firefox()
     browser.get('https://users.nexusmods.com/auth/continue?client_id=nexus&redirect_uri=https://www.nexusmods.com/oauth/callback&response_type=code&referrer=//www.nexusmods.com/darksouls3/mods/310?tab=files')
@@ -35,16 +46,41 @@ if (data['version'] != fileContents):
     password = browser.find_element_by_id('password')
     password.send_keys(config.password + Keys.RETURN)
 
-    WebDriverWait(browser).until(lambda d:
+    wait = WebDriverWait(browser, 10, poll_frequency=1)
+    wait.until(lambda d:
         d.find_elements_by_css_selector("#mainContent")
     )
     for id in ids:
         browser.get('https://www.nexusmods.com/darksouls3/mods/310?tab=files&file_id=' + str(id))
-        
-    
-    # cinderFiles = requests.get('https://www.nexusmods.com/darksouls3/mods/310?tab=files&file_id=' + str(ids[0]))
-    # soup = BeautifulSoup(cinderFiles.content, 'html.parser')
-    # elem = soup.find('div', class_='container')
-    # print(elem.prettify())
+        downloadBtn = wait.until(lambda d:
+            d.find_element_by_css_selector("#slowDownloadButton")
+        )
+        downloadBtn.click()
+        download = wait.until(lambda d: 
+            d.find_element_by_css_selector("p > a")
+        )
+        href = download.get_attribute('href')
+        local_filename = href.split('/')[-1].split('?')[0].replace('%20', ' ')
+        browser.close()
 
-    # authenticity_token=qIYGWJX6pfjTES3kCX9GhVqKSMSdTb8KoeqZ%2FpyKlpWQ6v0AFroqagO%2BgI%2FQizOD3I4fcpkJyUmMGEVFZ3MKQw%3D%3D&user%5Blogin%5D=thorzx&user%5Bpassword%5D=Thorxz111111&commit=Log+in
+        with requests.get(href, stream = True) as r:
+            r.raise_for_status()
+            with open(local_filename, 'wb') as f:
+                total = int(r.headers.get('content-length'))
+                downloaded = 0
+                for chunk in r.iter_content(chunk_size=8192):
+                    downloaded += len(chunk)
+                    f.write(chunk)
+                    done = int(50*downloaded/total)
+                    sys.stdout.write('\r[{}{}]'.format('█' * done, '.' * (50-done)))
+                    sys.stdout.flush()
+            sys.stdout.write('\n')
+
+        with zipfile.ZipFile(local_filename, 'r') as zip_ref:
+            zip_ref.extractall(config.unzip)
+        
+        configParser = configparser.ConfigParser()
+        configParser.read(config.unzip + '/modengine.ini')
+        configParser.set('online', 'blockNetworkAccess', "0")
+        with open(config.unzip + '/modengine.ini', 'w') as f:
+            configParser.write(f)
